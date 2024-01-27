db8b521ac8b51e9a7bced59d70fe30a53463178e
DREADEYE <134378619+DREADEYE-XD@users.noreply.github.com>
2024-01-27 18:31:37 +0530

    up

--- Weather-App/frontend/src/components/bodyView/bodyComps/currWeatherComps/currWeather.jsx
+++ Weather-App/frontend/src/components/bodyView/bodyComps/currWeatherComps/currWeather.jsx
@@ -1,35 +1,35 @@
 import React from "react";
 import "./currWeather.css";
-// import ForecastIcns from "./forecastIcns";
+import ForecastIcns from "./forecastIcns";
 
 /*
 *For icon docs
 https://openweathermap.org/weather-conditions#Weather-Condition-Codes-2
 */
 
-const CurrWeather = (props) => {
-
+const CurrWeather = ({ name, country, icon, weatherDesc, temp, temp_max, temp_min, feels_like, averageTemps }) => {
+  console.log(averageTemps)
   return (
     <div className="curr-weather-cont">
       <div className="loc-cont">
         <img src="./icons/location.png" alt="" height="25" width="25" />
-        <h1 className="cityName">{props.name}</h1>
+        <h1 className="cityName">{name}</h1>
       </div>
 
-      <h3 className="city-loc">{props.country}</h3>
+      <h3 className="city-loc">{country}</h3>
 
       <img
         className="currWthrIcn"
-        src={"https://openweathermap.org/img/wn/"+ props.icon + "@2x.png"}
+        src={"https://openweathermap.org/img/wn/"+ icon + "@2x.png"}
         alt=""
         height="130"
         width="130"
       />
 
-      <h3 className="wthr-desc">{props.weatherDesc}</h3>
+      <h3 className="wthr-desc">{weatherDesc}</h3>
 
       <div className="temp-cont">
-        <h2 className="temp">{Math.round(props.temp)}°</h2>
+        <h2 className="temp">{Math.round(temp)}°</h2>
 
         <div className="min-max">
           <img
@@ -39,7 +39,7 @@ const CurrWeather = (props) => {
             height="14"
             width="14"
           />
-          <span>{Math.round(props.temp_max)}°</span>
+          <span>{Math.round(temp_max)}°</span>
           <img
             className="min-temp"
             src="./icons/up-arrow.gif"
@@ -47,17 +47,18 @@ const CurrWeather = (props) => {
             height="14"
             width="14"
           />
-          <span>{Math.round(props.temp_min)}°</span>
+          <span>{Math.round(temp_min)}°</span>
         </div>
       </div>
 
       <ul className="forecast-icns">
-        {/* <ForecastIcns day={day} icn={icnLink} temp={foreTemp} />
-        <ForecastIcns day={day} icn={icnLink} temp={foreTemp} />
-        <ForecastIcns day={day} icn={icnLink} temp={foreTemp} />
-        <ForecastIcns day={day} icn={icnLink} temp={foreTemp} /> */}
+        <ForecastIcns date={averageTemps[1].date} icn={averageTemps[1].icon} />
+        <ForecastIcns date={averageTemps[2].date} icn={averageTemps[2].icon} />
+        <ForecastIcns date={averageTemps[3].date} icn={averageTemps[3].icon} />
+        <ForecastIcns date={averageTemps[4].date} icn={averageTemps[4].icon} />
+        <ForecastIcns date={averageTemps[5].date} icn={averageTemps[5].icon} />
       </ul>
-      <p className="forecast-desc">{"Feels Like " + Math.round(props.feels_like) + "°"}</p>
+      <p className="forecast-desc">{"Feels Like " + Math.round(feels_like) + "°"}</p>
     </div>
   );
 };
--- Weather-App/frontend/src/components/bodyView/bodyView.jsx
+++ Weather-App/frontend/src/components/bodyView/bodyView.jsx
@@ -1,51 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./bodyView.css";
 import CurrWeather from "./bodyComps/currWeatherComps/currWeather";
 import axios from "axios";
+import { defaultCurrentWeatherData, defaultForecastData } from "./defaultData";
+
+// console.log("forecast: " + defaultForecastData[0].list[0].dt_txt);
 
 const appId = "aa06c5bd726a3a36aaa80f484396993b";
 const defaultURL = "https://api.openweathermap.org/data/2.5/";
-const defaultData = {
-  weather: [{ id: "", main: "", description: "", icon: "" }],
-
-  main: {
-    temp: "",
-    feels_like: "",
-    temp_min: "",
-    temp_max: "",
-    pressure: "",
-    humidity: "",
-    sea_level: "",
-    grnd_level: "",
-  },
-  wind: { speed: "", deg: "", gust: "" },
-  clouds: { all: "" },
-  dt: "",
-  sys: {
-    type: "",
-    id: "",
-    country: "",
-    sunrise: "",
-    sunset: "",
-  },
-  timezone: "",
-  id: "",
-  name: "",
-  cod: "",
-};
 
 const BodyView = () => {
   const [cityName, setCityName] = useState("");
-  const [weatherData, setWeatherData] = useState(defaultData);
-  const [forecastData, setForecastData] = useState(defaultData);
-
-  //const currWeatherAPI = ``;
+  const [weatherData, setWeatherData] = useState(defaultCurrentWeatherData);
+  const [forecastData, setForecastData] = useState(defaultForecastData[0]);
+  const [averageTemps, setAverageTemps] = useState([]);
 
   const searchedData = () => {
     axios
-      .get(
-        `${defaultURL}weather?q=${cityName}&appid=${appId}&units=metric`
-      )
+      .get(`${defaultURL}weather?q=${cityName}&appid=${appId}&units=metric`)
       .then(async (response) => {
         setWeatherData(response.data);
       })
@@ -53,21 +25,88 @@ const BodyView = () => {
         console.log(error);
       });
 
-      //forecast?q={city name}&appid={API key}
-
     axios
-      .get(
-        `${defaultURL}forecast?q=${cityName}&appid=${appId}&units=metric`
-      )
+      .get(`${defaultURL}forecast?q=${cityName}&appid=${appId}&units=metric`)
       .then(async (response) => {
         setForecastData(response.data);
       })
       .catch((error) => {
         console.log(error);
-      }); 
+      });
   };
+  //console.log(forecastData.list[0].dt_txt);
+  useEffect(() => {
+    const calculateAverageTemps = () => {
+      const dailyTemps = {};
 
-  console.log(forecastData);
+      forecastData.list.forEach((forecast) => {
+        const rawDate = new Date(forecast.dt_txt);
+        const date = `${rawDate.getDate()}/${rawDate.getMonth() + 1}`;
+
+        if (!dailyTemps[date]) {
+          dailyTemps[date] = {
+            temps: [forecast.main.temp],
+            feels_like: [forecast.main.feels_like],
+            temp_min: [forecast.main.temp_min],
+            temp_max: [forecast.main.temp_max],
+            pressure: [forecast.main.pressure],
+            humidity: [forecast.main.humidity],
+            descriptions: [forecast.weather[0].description],
+            icons: [forecast.weather[0].icon],
+            wind_speed: [forecast.wind.speed],
+            wind_deg: [forecast.wind.deg],
+            wind_gust: [forecast.wind.gust],
+          };
+        } else {
+          dailyTemps[date].temps.push(forecast.main.temp);
+          dailyTemps[date].feels_like.push(forecast.main.feels_like);
+          dailyTemps[date].temp_min.push(forecast.main.temp_min);
+          dailyTemps[date].temp_max.push(forecast.main.temp_max);
+          dailyTemps[date].pressure.push(forecast.main.pressure);
+          dailyTemps[date].humidity.push(forecast.main.humidity);
+          dailyTemps[date].descriptions.push(forecast.weather[0].description);
+          dailyTemps[date].icons.push(forecast.weather[0].icon);
+          dailyTemps[date].wind_speed.push(forecast.wind.speed);
+          dailyTemps[date].wind_deg.push(forecast.wind.deg);
+          dailyTemps[date].wind_gust.push(forecast.wind.gust);
+        }
+      });
+
+      const averages = Object.entries(dailyTemps).map(([date, data]) => ({
+        date,
+        averageTemp:
+          data.temps.reduce((sum, temp) => sum + temp, 0) / data.temps.length,
+        feels_like:
+          data.feels_like.reduce((sum, feel) => sum + feel, 0) /
+          data.feels_like.length,
+        temp_min: Math.min(...data.temp_min),
+        temp_max: Math.max(...data.temp_max),
+        pressure:
+          data.pressure.reduce((sum, press) => sum + press, 0) /
+          data.pressure.length,
+        humidity:
+          data.humidity.reduce((sum, humid) => sum + humid, 0) /
+          data.humidity.length,
+        description: data.descriptions[0], // Assuming the weather description is the same for the day
+        icon: data.icons[0], // Assuming the weather icon is the same for the day
+        wind_speed:
+          data.wind_speed.reduce((sum, speed) => sum + speed, 0) /
+          data.wind_speed.length,
+        wind_deg:
+          data.wind_deg.reduce((sum, deg) => sum + deg, 0) /
+          data.wind_deg.length,
+        wind_gust: Math.max(...data.wind_gust),
+      }));
+
+      setAverageTemps(averages);
+    };
+
+    if (forecastData.list) {
+      calculateAverageTemps();
+    }
+  }, [forecastData]);
+
+  console.log(averageTemps);
 
   return (
     <body>
@@ -79,7 +118,7 @@ const BodyView = () => {
         }}
       />
       <button onClick={searchedData}>Submit</button>
-      <CurrWeather 
+      <CurrWeather
         name={weatherData.name}
         country={weatherData.sys.country}
         icon={weatherData.weather[0].icon}
@@ -88,6 +127,7 @@ const BodyView = () => {
         temp_max={weatherData.main.temp_max}
         temp_min={weatherData.main.temp_min}
         feels_like={weatherData.main.feels_like}
+        averageTemps={averageTemps}
       />
     </body>
   );
