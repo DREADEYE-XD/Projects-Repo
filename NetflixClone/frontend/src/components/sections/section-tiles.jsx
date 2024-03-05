c588dcb9f7ec739168e40c513d0e42771d9d8cee
DREADEYE-XD <134378619+DREADEYE-XD@users.noreply.github.com>
2024-03-05 19:02:44 +0530

    u

--- NetflixClone/frontend/src/components/sections/section-tiles.jsx
+++ NetflixClone/frontend/src/components/sections/section-tiles.jsx
@@ -1,41 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import CategoryMvTiles from "./category-mv-tiles";
 import RightArrowIcon from "../nav/sidebar/sidebar-layer/right-arrow-icon";
 
 const SectionTiles = ({
   categoryHeading,
   recentlyAdded,
+  movieData,
   newEpisodeAvailable,
+  imageUrl,
 }) => {
-  const baseUrl = "https://api.themoviedb.org/3/";
-  const popularMovieUrl = "movie/popular?language=en-US&page=1";
-  const imageUrl = "https://image.tmdb.org/t/p/w500";
-  const [popularMovie, setPopularMovie] = useState([]);
-
-  // Fetch popular movies data
-  useEffect(() => {
-    const options = {
-      method: "GET",
-      headers: {
-        accept: "application/json",
-        Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YTA4YjU0ZjYzMzg0YzcxZGZjMDJiMzMxYWU0YTEwZiIsInN1YiI6IjY1ZTFhMWY4YTI4NGViMDE2NGQ0YWE5ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Y43951zZOqOXC2nP-4FUYK1vMFu5sFQ0wKH4Ei4o61Y",
-      },
-    };
-
-    const fetchPopularMovies = async () => {
-      try {
-        const response = await fetch(`${baseUrl + popularMovieUrl}`, options);
-        const data = await response.json();
-        console.log(data.results);
-        setPopularMovie(data.results); // Assuming 'results' is the array of movies
-      } catch (error) {
-        console.error("Error fetching popular movies:", error);
-      }
-    };
-
-    fetchPopularMovies();
-  }, []);
 
   return (
     <div className="section-tiles">
@@ -48,15 +21,17 @@ const SectionTiles = ({
           <RightArrowIcon />
         </div>
 
-        {popularMovie.map((movie, index) => (
-          <CategoryMvTiles
-            key={index}
-            movieThumbnail={imageUrl + movie.backdrop_path}
-            recentlyAdded={recentlyAdded}
-            newEpisodeAvailable={newEpisodeAvailable}
-            movieName={movie.original_title}
-          />
-        ))}
+        {movieData &&
+          Object.keys(movieData).length > 0 &&
+          movieData.map((movie, index) => (
+            <CategoryMvTiles
+              key={index}
+              movieThumbnail={imageUrl + movie.backdrop_path}
+              recentlyAdded={recentlyAdded}
+              newEpisodeAvailable={newEpisodeAvailable}
+              movieName={movie.original_title}
+            />  
+          ))}
 
         <div className="slider rightArrow">
           <RightArrowIcon />
--- NetflixClone/frontend/src/components/sections/sections.jsx
+++ NetflixClone/frontend/src/components/sections/sections.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ContinueWatching from "./continue-watching";
 import "./sections.css";
 import SectionTiles from "./section-tiles";
+import axios from "axios";
 const Sections = () => {
   const movieImages = [
     "./images/movie-tiles/avatar.jpg",
@@ -14,6 +15,73 @@ const Sections = () => {
     "./images/movie-tiles/world-warz.jpg",
   ];
 
+  const baseUrl = "https://api.themoviedb.org/3/";
+  const popularMovieUrl = "movie/popular?language=en-US&page=1";
+  const topRatedMovieUrl = "movie/top_rated?language=en-US&page=1";
+  const upcomingMovieUrl = "movie/upcoming?language=en-US&page=1";
+  const imageUrl = "https://image.tmdb.org/t/p/w500";
+  const [popularMovie, setPopularMovie] = useState({});
+  const [topRatedMovie, setTopRatedMovie] = useState({});
+  const [upcomingMovie, setUpcomingMovie] = useState({});
+
+  // Fetch popular movies data
+  useEffect(() => {
+    axios
+      .get(`${baseUrl + popularMovieUrl}`, {
+        headers: {
+          accept: "application/json",
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YTA4YjU0ZjYzMzg0YzcxZGZjMDJiMzMxYWU0YTEwZiIsInN1YiI6IjY1ZTFhMWY4YTI4NGViMDE2NGQ0YWE5ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Y43951zZOqOXC2nP-4FUYK1vMFu5sFQ0wKH4Ei4o61Y",
+        },
+      })
+      .then(async (response) => {
+        const data = response.data.results;
+        console.log(response);
+        setPopularMovie(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    axios
+      .get(`${baseUrl + topRatedMovieUrl}`, {
+        headers: {
+          accept: "application/json",
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YTA4YjU0ZjYzMzg0YzcxZGZjMDJiMzMxYWU0YTEwZiIsInN1YiI6IjY1ZTFhMWY4YTI4NGViMDE2NGQ0YWE5ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Y43951zZOqOXC2nP-4FUYK1vMFu5sFQ0wKH4Ei4o61Y",
+        },
+      })
+      .then(async (response) => {
+        const data = response.data.results;
+        console.log(response);
+        setTopRatedMovie(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    axios
+      .get(`${baseUrl + upcomingMovieUrl}`, {
+        headers: {
+          accept: "application/json",
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YTA4YjU0ZjYzMzg0YzcxZGZjMDJiMzMxYWU0YTEwZiIsInN1YiI6IjY1ZTFhMWY4YTI4NGViMDE2NGQ0YWE5ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Y43951zZOqOXC2nP-4FUYK1vMFu5sFQ0wKH4Ei4o61Y",
+        },
+      })
+      .then(async (response) => {
+        const data = response.data.results;
+        console.log(response);
+        setUpcomingMovie(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    // fetchPopularMovies();
+  }, []);
+
+  console.log(popularMovie);
+
   return (
     <div className="sections">
       <section>
@@ -21,30 +89,32 @@ const Sections = () => {
       </section>
 
       <section>
-      
-          <SectionTiles
-            categoryHeading="Only on Netflix(Clone)"
-            recentlyAdded={false}
-            newEpisodeAvailable={false}
-          />
-       
-      </section>
-
-      <section>
         <SectionTiles
-          categoryHeading="Adventure Movies"
-          movieThumbnail={movieImages}
+          categoryHeading="Popular Movies"
           recentlyAdded={false}
-          newEpisodeAvailable={true}
+          newEpisodeAvailable={false}
+          movieData={popularMovie}
+          imageUrl={imageUrl}
         />
       </section>
 
       <section>
         <SectionTiles
-          categoryHeading="Action Movies"
-          movieThumbnail={movieImages}
+          categoryHeading="Top Rated Movies"
+          recentlyAdded={false}
+          newEpisodeAvailable={true}
+          movieData={topRatedMovie}
+          imageUrl={imageUrl}
+        />
+      </section>
+
+      <section>
+        <SectionTiles
+          categoryHeading="Upcoming Movies"
           recentlyAdded={true}
           newEpisodeAvailable={false}
+          movieData={upcomingMovie}
+          imageUrl={imageUrl}
         />
       </section>
     </div>
